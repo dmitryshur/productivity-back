e6384c799a6843a54b6788adf0e95b7cbcf9de80
dmitryshur <dimashur@gmail.com>
2020-05-12 22:26:37 +0300

    Change main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate log;
 
+use actix_rt;
 use actix_web::{middleware, web, App, HttpServer};
 use postgres::{self, NoTls};
 use productivity::account::account_controllers::{account_login, account_register};
@@ -37,8 +38,7 @@ async fn create_redis_client() -> redis::RedisResult<redis::aio::Connection> {
     connection
 }
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "warnings=warn,actix_web=info");
     std::env::set_var("RUST_BACKTRACE", "1");
 
@@ -55,36 +55,38 @@ async fn main() -> std::io::Result<()> {
         }
     };
 
-    let redis_client = match create_redis_client().await {
-        Ok(client) => Arc::new(Mutex::new(client)),
-        Err(err) => {
-            warn!(target: "warnings", "Warn: {:?}", err);
-            panic!(err);
-        }
-    };
+    actix_rt::System::new("Main system".to_string()).block_on(async move {
+        let redis_client = match create_redis_client().await {
+            Ok(client) => Arc::new(Mutex::new(client)),
+            Err(err) => {
+                warn!(target: "warnings", "Warn: {:?}", err);
+                panic!(err);
+            }
+        };
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .data(AppState {
-                db_pool: db_pool.clone(),
-                redis_client: redis_client.clone(),
-            })
-            .service(
-                web::scope("/api/todo")
-                    .wrap(middlewares::auth::Authentication)
-                    .route("/create", web::post().to(todo_create))
-                    .route("/get", web::get().to(todo_get))
-                    .route("/edit", web::post().to(todo_edit))
-                    .route("/delete", web::post().to(todo_delete)),
-            )
-            .service(
-                web::scope("/api/account")
-                    .route("/register", web::post().to(account_register))
-                    .route("/login", web::post().to(account_login)),
-            )
+        HttpServer::new(move || {
+            App::new()
+                .wrap(middleware::Logger::default())
+                .data(AppState {
+                    db_pool: db_pool.clone(),
+                    redis_client: redis_client.clone(),
+                })
+                .service(
+                    web::scope("/api/todo")
+                        .wrap(middlewares::auth::Authentication)
+                        .route("/create", web::post().to(todo_create))
+                        .route("/get", web::get().to(todo_get))
+                        .route("/edit", web::post().to(todo_edit))
+                        .route("/delete", web::post().to(todo_delete)),
+                )
+                .service(
+                    web::scope("/api/account")
+                        .route("/register", web::post().to(account_register))
+                        .route("/login", web::post().to(account_login)),
+                )
+        })
+        .bind(format!("{}:{}", host, port))?
+        .run()
+        .await
     })
-    .bind(format!("{}:{}", host, port))?
-    .run()
-    .await
 }
